8a4c5b5a18080d171e8a1d1b4c8aa3af026225aa
DaDevMate <67144384+dev365b@users.noreply.github.com>
2023-02-04 18:39:10 +0800

    Initial Commit

---
+++ Friday/FridayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridayApp.swift
+//  Friday
+//
+//  Created by Nethan on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct FridayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
